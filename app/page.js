9e128730ee444e0c5eab0b717448a5e9bad345fa
Niranjan <niranjan41210@gmail.com>
2023-12-10 21:54:58 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -4,13 +4,13 @@ import { WelcomeSection, AboutSection, TechnologiesSection } from "@/app/section
 export default function Page() {
   return (
     <div className="container-md">
-      <Head>
-        <script
-          src="https://beamanalytics.b-cdn.net/beam.min.js"
-          data-token="cea78beb-0056-47f4-8382-00f1ed721ce1"
-          async
-        />
-      </Head>
+		<Head>
+			<script
+			src="https://beamanalytics.b-cdn.net/beam.min.js"
+			data-token="cea78beb-0056-47f4-8382-00f1ed721ce1"
+			async
+			/>
+		</Head>
       
       <WelcomeSection />
       <AboutSection />
