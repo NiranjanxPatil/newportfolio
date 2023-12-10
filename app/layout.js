e2266a1951d8c773c12d4dec91769b2fe7a9c820
Niranjan <niranjan41210@gmail.com>
2023-12-10 22:07:07 +0530

    beam code

--- app/layout.js
+++ app/layout.js
@@ -10,12 +10,11 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			
-        <script
+			<script
           src="https://beamanalytics.b-cdn.net/beam.min.js"
           data-token="cea78beb-0056-47f4-8382-00f1ed721ce1"
           async
         />
-    
 			<body>
 				<ThemeContext>
 					<AppHeader />
--- app/page.js
+++ app/page.js
@@ -2,17 +2,21 @@ import Head from 'next/head';
 import { WelcomeSection, AboutSection, TechnologiesSection } from "@/app/sections";
 
 export default function Page() {
+	<script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token="cea78beb-0056-47f4-8382-00f1ed721ce1"
+          async
+        />
   return (
     <div className="container-md">
-		<Head>
-			<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token="cea78beb-0056-47f4-8382-00f1ed721ce1"
-			async
-			/>
-		</Head>
-      
-      <WelcomeSection />
+      <Head>
+	  <script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token="cea78beb-0056-47f4-8382-00f1ed721ce1"
+          async
+        />
+      </Head>      
+      <WelcomeSection />	
       <AboutSection />
     </div>
   );
