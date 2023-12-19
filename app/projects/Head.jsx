05b941d7b13fc01ce2d6a0d1a0a63240f4d432b7
Niranjan <niranjan41210@gmail.com>
2023-12-20 01:58:06 +0530

    error

--- app/projects/Head.jsx
+++ app/projects/Head.jsx
@@ -1,29 +1,24 @@
-import React from 'react'
-import { Header } from 'next/dist/lib/load-custom-routes'
-import Link from "next/link";
+// Head.js
+import React from 'react';
+import { Link } from 'lucide-react';
 
 const ProjectHeader = () => {
   return (
-    <header className="pt-5 pb-5 sticky top-0 z-10 bg-inherit shadow-sm bg-slate-800 ">
-			<div className="container-md">
-				<div className="flex justify-between items-center gap-3 text-white  ">
-					<h1 className='text-4xl font-semibold'>
-						PROJECT SECTION  	
-					</h1>
-					
-					<Link
-								href="@/app"
-								tabIndex="0"
-								className="btn"
-								aria-label="Latest projects"
-							>
-								baccck
-							</Link>
-					
-				</div>
-			</div>
-		</header>
-  )
+    <header className="pt-5 pb-5 sticky top-0 z-10 bg-inherit shadow-sm bg-slate-800">
+      <div className="container-md">
+        <div className="flex justify-between items-center gap-3 text-white">
+          <h1 className='text-4xl font-semibold'>
+            PROJECT SECTION
+          </h1>
+          <div>
+            <Link href='sections'>
+              Home Page
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default ProjectHeader
+export default ProjectHeader;
