a31629466c0759fa676f721d24bf6c757dc64315
Niranjan <niranjan41210@gmail.com>
2024-01-23 16:23:43 +0530

    chnages

--- app/sections/about/index.jsx
+++ app/sections/about/index.jsx
@@ -9,13 +9,6 @@ export function AboutSection() {
 	const ref = useRef(null);
 	const isInView = useInView(ref, { once: true });
 
-  const Introduction = () => {
-    const [hovered, setHovered] = React.useState(false);
-  
-    const handleHover = () => {
-      setHovered(!hovered);
-    };
-  }
 	return (
 		<LazyMotion features={domAnimation}>
 			<section id="about" className="section">
@@ -31,40 +24,25 @@ export function AboutSection() {
     transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
   }}
 >
-<p>
-          Hey there! Im <strong>Niranjan </strong>, a tech enthusiast skilled in <strong>Designe</strong>, <strong>Development</strong>,  and <strong>OPS</strong>.
-        </p>
-      </div>
-
-      <div className={`intro-paragraph ${hovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
-        <p>
-          Currently on an exciting journey in AIML engineering at <strong>Mumbai University</strong>, with a diploma in Computer Engineering from <strong>Pune University</strong>.
-          Outside of coding, you can catch me diving into blogs—both reading and writing on my personal website <a href="https://niranjann.tech" target="_blank" rel="noopener noreferrer">niranjann.tech</a>.
-        </p>
-      </div>
-
-      <div>
-        <p>
-          My programming adventure officially kicked off in 2019 as a Java developer.
-        </p>
-      </div>
-
-      <div className="intro-paragraph">
-        <p>
-          Thrived in group leadership and project management in my past roles, and Im particularly proud of my achievements in mobile app development during my Diploma years.
-        </p>
-      </div>
-
-      <div>
-        <p>
-          Bringing a blend of expertise in cloud, DevOps, and AI/ML to the table, Im equally passionate about crafting designs in Figma and Canva.
-        </p>
-      </div>
-
-      <div className={`intro-paragraph ${hovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
-        <p>
-          Currently, Im laser-focused on honing my cloud and DevOps skills. Eager to collaborate on projects that harness my knowledge in AIML and AI tools. Lets build something amazing together!
-        </p>
+  <p>
+  Hello, Im Niranjan Patil, skilled Designe Development and Deployments.
+  </p>
+  <p className="my-3.5">
+  Im currently pursuing AIML engineering at Mumbai University and hold a Diploma in Computer Engineering from Pune University. 
+  Outside of work, I enjoy reading blogs and writing blogs on my personal website niranjann.tech.
+  </p>
+  <p>My programming journey officially began in 2019 as a java developer.</p>
+  <p className="my-3.5">
+  In my past roles, I excelled in group leadership and project management, and 
+  Im proud of my achievements in mobile app development during my Diploma years.
+  </p>
+  <p>
+  I bring to the table expertise in cloud, DevOps, and AI/ML, along with a passion for designing in Figma and Canva.
+  </p>
+  <p className="my-3.5">
+  Currently, Im focused on honing my cloud and DevOps skills, and 
+  Im eager to collaborate on projects that leverage my knowledge in AIML and AI toolss.
+  </p>
 </div>
 
 				</div>
